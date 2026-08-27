375c68aedaf9f12cffcd29cbf8f10fc7949fa8a5
zhaoyu <zhaoyuzhuo42@gmail.com>
2026-08-27 23:43:41 +0100

    feat: add get events integration test

--- tests/test_router.py
+++ tests/test_router.py
@@ -1,13 +1,30 @@
 import pytest
+import json
 from fastapi.testclient import TestClient
-from db.router import get_all_events
+from main import app
+
 
 @pytest.fixture
 def client():
     return TestClient(app)
 
-def test_get_all_events_returns_200_and_seeded_events(client):
+def test_get_all_events_returns_200(client):
     response = client.get("/api/events")
     assert response.status_code == 200
-    titles = [event["title"] for event in response.json()["events"]]
-    assert "Leeds Tech Meetup - June Edition" in titles
+
+def test_get_all_events_get_events(client):
+    response = client.get("/api/events")
+    body = response.json()
+    assert "events" in body
+
+def test_get_all_events_get_events(client):
+    response = client.get("/api/events")
+    with open ("db/data/events.json") as f:
+        seeded_events =  json.load(f)
+        seeded_title = [event["title"]
+                        for event in seeded_events]
+    results = response.json()["events"]
+    result_title = [result["title"]
+                    for result in results]
+    assert len(seeded_events) == len(results)
+    assert sorted(seeded_title) == sorted(result_title)
