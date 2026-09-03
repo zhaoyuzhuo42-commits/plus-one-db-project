6b77efad0843fcbefc0023c0d4af0f3766fbfa79
zhaoyu <zhaoyuzhuo42@gmail.com>
2026-09-03 22:16:30 +0100

    add: add test for get event by event id endpoint

--- tests/test_router.py
+++ tests/test_router.py
@@ -8,6 +8,8 @@ from main import app
 def client():
     return TestClient(app)
 
+
+#get events
 def test_get_all_events_returns_200(client):
     response = client.get("/api/events")
     assert response.status_code == 200
@@ -28,3 +30,26 @@ def test_get_all_events_get_all_events(client):
                     for result in results]
     assert len(seeded_events) == len(results)
     assert sorted(seeded_title) == sorted(result_title)
+
+
+#get event_id
+def test_get_event_id_returns_200(client):
+    reponse = client.get("/api/events/1")
+    assert reponse.status_code == 200
+
+def test_get_event_id_returns_correct_event(client):
+    with open ("db/data/events.json") as f:
+        seed_events = json.load(f)
+    reponse = client.get("/api/events/1")
+    result = reponse.json()["event"]
+    assert result["id"] == 1
+    assert result["title"] == seed_events[0]["title"]
+
+
+def test_get_event_id_not_found_returns_404(client):
+    reponse = client.get("/api/events/999")
+    assert reponse.status_code == 404
+
+def test_get_event_id_not_intger_returns_400(client):
+    response = client.get("/api/events/hello")
+    assert response.status_code == 400
